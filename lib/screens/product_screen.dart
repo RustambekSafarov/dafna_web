3d3db7c35f5a999c88ed591a064d5d8e1cdd2e9a
RustambekSafarov <rustambeksafarovv@gmail.com>
2023-03-09 23:02:05 +0500

    sort

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -112,114 +112,196 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                       return Padding(
                         padding:
                             const EdgeInsets.only(left: 15, right: 65, top: 20),
-                        child: GridView.builder(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: snapshot
-                                .data!['prodouct_type']['prodoucts'].length,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 4,
-                                    childAspectRatio: 0.5,
-                                    mainAxisSpacing: 20,
-                                    crossAxisSpacing: 20),
-                            itemBuilder: (context, index) {
-                              widget.productTypeId =
-                                  snapshot.data!['prodouct_type']['prodoucts']
-                                      [index]['id'];
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Text('Saralash turi:'),
+                                InkWell(
+                                  highlightColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  splashColor: Colors.transparent,
+                                  onTap: () {},
+                                  child: Container(
+                                    margin: EdgeInsets.all(5),
+                                    height: 35,
+                                    width: 85,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.blue,
+                                    ),
+                                    child: Center(child: Text('Mashhurlik')),
+                                  ),
+                                ),
+                                InkWell(
+                                  highlightColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  splashColor: Colors.transparent,
+                                  onTap: () {},
+                                  child: Container(
+                                    margin: EdgeInsets.all(5),
+                                    height: 35,
+                                    width: 65,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.blue,
+                                    ),
+                                    child: Center(child: Text('Yangi')),
+                                  ),
+                                ),
+                                InkWell(
+                                  highlightColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  splashColor: Colors.transparent,
+                                  onTap: () {},
+                                  child: Container(
+                                    margin: EdgeInsets.all(5),
+                                    height: 35,
+                                    width: 65,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.blue,
+                                    ),
+                                    child: Center(child: Text('Narxi')),
+                                  ),
+                                ),
+                                InkWell(
+                                  highlightColor: Colors.transparent,
+                                  hoverColor: Colors.transparent,
+                                  splashColor: Colors.transparent,
+                                  onTap: () {},
+                                  child: Container(
+                                    margin: EdgeInsets.all(5),
+                                    height: 35,
+                                    width: 65,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color: Colors.blue,
+                                    ),
+                                    child: Center(child: Text('Chegirma')),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            GridView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: snapshot
+                                    .data!['prodouct_type']['prodoucts'].length,
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 4,
+                                        childAspectRatio: 0.5,
+                                        mainAxisSpacing: 20,
+                                        crossAxisSpacing: 20),
+                                itemBuilder: (context, index) {
+                                  widget.productTypeId =
+                                      snapshot.data!['prodouct_type']
+                                          ['prodoucts'][index]['id'];
 
-                              return InkWell(
-                                hoverColor: Colors.transparent,
-                                highlightColor: Colors.transparent,
-                                splashColor: Colors.transparent,
-                                onTap: () {
-                                  context.goNamed('/product-info',
-                                      extra: widget.productTypeId);
-                                },
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(15),
-                                      child: SizedBox(
-                                        height: 220,
-                                        child: Stack(
-                                          children: [
-                                            Positioned(
-                                              top: 0,
-                                              right: 0,
-                                              child: IconButton(
-                                                onPressed: () {},
-                                                icon:
-                                                    Icon(Icons.favorite_border),
-                                              ),
-                                            ),
-                                            Image.network(
-                                              'https://ogabek007.pythonanywhere.com/' +
-                                                  snapshot.data![
-                                                              'prodouct_type']
-                                                          ['prodoucts'][index]
-                                                      ['img_url'],
-                                              fit: BoxFit.cover,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 70,
-                                      child: Text(
-                                        snapshot.data!['prodouct_type']
-                                                ['prodoucts'][index]['name']
-                                            .toUpperCase(),
-                                        textAlign: TextAlign.center,
-                                        style: const TextStyle(
-                                            fontSize: 15.5,
-                                            fontWeight: FontWeight.w900),
-                                      ),
-                                    ),
-                                    Container(
-                                      height: 100,
-                                      width: 220,
-                                      // padding: const EdgeInsets.all(13.0),
-                                      child: Text(
-                                        snapshot.data!['prodouct_type']
-                                            ['prodoucts'][index]['discrpition'],
-                                        textAlign: TextAlign.center,
-                                        style: const TextStyle(
-                                            fontSize: 13,
-                                            fontWeight: FontWeight.w300,
-                                            color: Colors.grey),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(13.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            '${snapshot.data!['prodouct_type']['prodoucts'][index]['price']}',
-                                            textAlign: TextAlign.center,
-                                            style: const TextStyle(
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.w700,
+                                  return InkWell(
+                                    hoverColor: Colors.transparent,
+                                    highlightColor: Colors.transparent,
+                                    splashColor: Colors.transparent,
+                                    onTap: () {
+                                      context.goNamed('/product-info',
+                                          extra: widget.productTypeId);
+                                    },
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          child: SizedBox(
+                                            height: 220,
+                                            child: Stack(
+                                              children: [
+                                                Positioned(
+                                                  top: 0,
+                                                  right: 0,
+                                                  child: IconButton(
+                                                    onPressed: () {},
+                                                    icon: Icon(
+                                                        Icons.favorite_border),
+                                                  ),
+                                                ),
+                                                Image.network(
+                                                  'https://ogabek007.pythonanywhere.com/' +
+                                                      snapshot.data![
+                                                                  'prodouct_type']
+                                                              ['prodoucts']
+                                                          [index]['img_url'],
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ],
                                             ),
                                           ),
-                                          const Text(
-                                            ' so\'m',
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w700,
-                                            ),
-                                          )
-                                        ],
-                                      ),
+                                        ),
+                                        SizedBox(
+                                          height: 70,
+                                          child: Text(
+                                            snapshot.data!['prodouct_type']
+                                                    ['prodoucts'][index]['name']
+                                                .toUpperCase(),
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                                fontSize: 15.5,
+                                                fontWeight: FontWeight.w900),
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 100,
+                                          width: 220,
+                                          // padding: const EdgeInsets.all(13.0),
+                                          child: Text(
+                                            snapshot.data!['prodouct_type']
+                                                    ['prodoucts'][index]
+                                                ['discrpition'],
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                                fontSize: 13,
+                                                fontWeight: FontWeight.w300,
+                                                color: Colors.grey),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(13.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                '${snapshot.data!['prodouct_type']['prodoucts'][index]['price']}',
+                                                textAlign: TextAlign.center,
+                                                style: const TextStyle(
+                                                  fontSize: 22,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              ),
+                                              const Text(
+                                                ' so\'m',
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                                  ],
-                                ),
-                              );
-                            }),
+                                  );
+                                }),
+                          ],
+                        ),
                       );
                     } else if (snapshot.connectionState ==
                         ConnectionState.waiting) {
