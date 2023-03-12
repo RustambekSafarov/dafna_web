d61531a354297d81101f0fda429f0552bd5cd0ae
RustambekSafarov <rustambeksafarovv@gmail.com>
2023-03-12 22:32:16 +0500

    sponsors

--- lib/widget/sponsors.dart
+++ lib/widget/sponsors.dart
@@ -25,30 +25,33 @@ class Sponsors extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Link(
-              target: LinkTarget.defaultTarget,
-              uri: Uri.parse('https://eman.uz/'),
-              builder: (context, followLink) => InkWell(
-                hoverColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                splashColor: Colors.transparent,
-                onTap: followLink,
-                child: Container(
-                  width: defaultTargetPlatform == TargetPlatform.android ||
-                          defaultTargetPlatform == TargetPlatform.iOS
-                      ? 200
-                      : 500,
-                  height: defaultTargetPlatform == TargetPlatform.android ||
-                          defaultTargetPlatform == TargetPlatform.iOS
-                      ? 130
-                      : 330,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: const DecorationImage(
-                      fit: BoxFit.fitHeight,
-                      image: NetworkImage(
-                          'https://mebel.dafna.uz/img/Today/eman-banner.png'),
-                    ),
+            InkWell(
+              hoverColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              onTap: () async {
+                const url = 'https://eman.uz/';
+                if (await canLaunchUrl(Uri.parse(url))) {
+                  await launchUrl(Uri.parse(url));
+                } else {
+                  throw 'Could not launch $url';
+                }
+              },
+              child: Container(
+                width: defaultTargetPlatform == TargetPlatform.android ||
+                        defaultTargetPlatform == TargetPlatform.iOS
+                    ? 200
+                    : 500,
+                height: defaultTargetPlatform == TargetPlatform.android ||
+                        defaultTargetPlatform == TargetPlatform.iOS
+                    ? 130
+                    : 330,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: const DecorationImage(
+                    fit: BoxFit.fitHeight,
+                    image: NetworkImage(
+                        'https://mebel.dafna.uz/img/Today/eman-banner.png'),
                   ),
                 ),
               ),
