8c787730ad9b98071fb5cece003f5e85b22c80c6
RustambekSafarov <rustambeksafarovv@gmail.com>
2023-03-28 23:06:07 +0500

    scrollbar

--- lib/web/screens/shopping_card_screen.dart
+++ lib/web/screens/shopping_card_screen.dart
@@ -58,112 +58,119 @@ class ShoppingCardScreen extends StatelessWidget {
               future: getCart(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Padding(
-                    padding:
-                        const EdgeInsets.only(left: 150, right: 150, top: 20),
-                    child: ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!['carts'].length,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          hoverColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          splashColor: Colors.transparent,
-                          onTap: () {
-                            context.goNamed(
-                              '/product-info',
-                              extra: snapshot.data!['carts'][index]['id'],
-                            );
-                          },
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Row(
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(15),
-                                    child: SizedBox(
-                                      height: 200,
-                                      width: 220,
-                                      child: Image.network(
-                                        'https://ogabek007.pythonanywhere.com/' +
-                                            snapshot.data!['carts'][index]
-                                                ['img_url'],
-                                        fit: BoxFit.cover,
-                                        loadingBuilder:
-                                            (context, child, loadingProgress) {
-                                          if (loadingProgress == null) {
-                                            return child;
-                                          }
-                                          return Image.network(
-                                            'https://telegra.ph/file/a775320534f348ae7f531.png',
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 70,
-                                    child: Text(
-                                      snapshot.data!['carts'][index]['name']
-                                          .toUpperCase(),
-                                      textAlign: TextAlign.center,
-                                      style: const TextStyle(
-                                          fontSize: 15.5,
-                                          fontWeight: FontWeight.w900),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Container(
-                                height: 120,
-                                padding: const EdgeInsets.all(13.0),
-                                child: Column(
+                  return snapshot.data!['carts'].isEmpty
+                      ? Center(
+                          child: Text('There is no product yet!'),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(
+                              left: 150, right: 150, top: 20),
+                          child: ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!['carts'].length,
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                hoverColor: Colors.transparent,
+                                highlightColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                onTap: () {
+                                  context.goNamed(
+                                    '/product-info',
+                                    extra: snapshot.data!['carts'][index]['id'],
+                                  );
+                                },
+                                child: Row(
                                   mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
                                     Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
                                       children: [
-                                        Text(
-                                          '${snapshot.data!['carts'][index]['price']}',
-                                          textAlign: TextAlign.center,
-                                          style: const TextStyle(
-                                            fontSize: 22,
-                                            fontWeight: FontWeight.w700,
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                          child: SizedBox(
+                                            height: 200,
+                                            width: 220,
+                                            child: Image.network(
+                                              'https://ogabek007.pythonanywhere.com/' +
+                                                  snapshot.data!['carts'][index]
+                                                      ['img_url'],
+                                              fit: BoxFit.cover,
+                                              loadingBuilder: (context, child,
+                                                  loadingProgress) {
+                                                if (loadingProgress == null) {
+                                                  return child;
+                                                }
+                                                return Image.network(
+                                                  'https://telegra.ph/file/a775320534f348ae7f531.png',
+                                                );
+                                              },
+                                            ),
                                           ),
                                         ),
-                                        const Text(
-                                          ' so\'m',
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w700,
+                                        SizedBox(
+                                          height: 70,
+                                          child: Text(
+                                            snapshot.data!['carts'][index]
+                                                    ['name']
+                                                .toUpperCase(),
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                                fontSize: 15.5,
+                                                fontWeight: FontWeight.w900),
                                           ),
-                                        )
+                                        ),
                                       ],
                                     ),
-                                    InkWell(
-                                      child: Row(
+                                    Container(
+                                      height: 120,
+                                      padding: const EdgeInsets.all(13.0),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
                                         children: [
-                                          Icon(Icons.delete),
-                                          Text(
-                                            'O\'chirish',
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                '${snapshot.data!['carts'][index]['price']}',
+                                                textAlign: TextAlign.center,
+                                                style: const TextStyle(
+                                                  fontSize: 22,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              ),
+                                              const Text(
+                                                ' so\'m',
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w700,
+                                                ),
+                                              )
+                                            ],
                                           ),
+                                          InkWell(
+                                            child: Row(
+                                              children: [
+                                                Icon(Icons.delete),
+                                                Text(
+                                                  'O\'chirish',
+                                                ),
+                                              ],
+                                            ),
+                                          )
                                         ],
                                       ),
-                                    )
+                                    ),
                                   ],
                                 ),
-                              ),
-                            ],
+                              );
+                            },
                           ),
                         );
-                      },
-                    ),
-                  );
                 } else if (snapshot.connectionState ==
                     ConnectionState.waiting) {
                   return const Center(
